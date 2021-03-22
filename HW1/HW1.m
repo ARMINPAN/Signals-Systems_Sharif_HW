b9ec93007025332f6d5f4fe19e86cbeb8ab8d800
Your Name <arminpp1379@gmail.com>
2021-03-22 16:34:59 +0430

    z transition

--- HW1/HW1.m
+++ HW1/HW1.m
@@ -1,17 +1,23 @@
 % all the questions are in this code and they`ve got seperated by different
 % sections in the code
-
+% functions are defined at the end of the code
 %%
-% Ques.1.1
+% Ques.1.1 and 1.2
+% 1.1
 % Wireworld - 2d_fourColor_Cellular Automation 
 % 0 - > empty , 1 - > conductor , 2 - > electron head , 3 - > electron tail
 % empty : white ,conductor : green, electron head : purple , electron tail : red 
 % I have defined a function which user can choose size of the table and
 % and number of generations
-
-
 WireWorld(20,200);
 
+%%
+% Ques.2.1
+% We want to calculate the Z Transition of the x[n] given below
+Z_Transform();
+%%
+%% all the functions
+%Question 1
 function WireWorld(size, gens)
     CurrentCells = zeros(size,size);
     nextStepCells = zeros(size,size);
@@ -20,7 +26,10 @@ function WireWorld(size, gens)
     title('Generation_0');
     
     %userInput - Initial condtion of the table
+    %if you want to give initial state to the matrix from here change the 
+    %CurrentCells below and comment Input function
     CurrentCells = Input(CurrentCells);
+    
     pause(0.01)
     
     while gens > 0
@@ -36,7 +45,6 @@ function WireWorld(size, gens)
                     if CurrentCells(x, y) == 3 	% electron tail to conductor
                         nextStepCells(x, y) = 1;
                     end
-                    display([x,gens,y]);
                     if CurrentCells(x, y) == 1 	% conductor to electron head or conductor
                         counter = 0;
                         for k = -1:1:1
@@ -76,7 +84,6 @@ function WireWorld(size, gens)
                                             counter = counter + 1;
                                         end   
                                     elseif(x ~= size && y ~= size && x ~= 1 && y ~= 1)
-                                        
                                         if (CurrentCells(x+k, y+j) == 2)
                                             counter = counter + 1;
                                         end  
@@ -101,7 +108,6 @@ function WireWorld(size, gens)
 		gens = gens - 1;
     end
 end
-
 function cells = Input(in)
     cells = in;
     while 1
@@ -127,7 +133,6 @@ function cells = Input(in)
         end
     end
 end
-
 function drawTable(cells)
     % creating the table
     sizeOfAxis = length(cells);
@@ -147,5 +152,66 @@ function drawTable(cells)
 		end
     end
 end
-%%
-% Ques.1.2
+
+%Question 2.1
+function Z_Transform()
+syms z
+syms n integer
+assume(n >= 0);
+syms x(n)
+x(n) = cos(n*pi/6);
+
+X(z) = ztrans(x); % Z transform of the x[n] in the command window
+% we use numden function and after that solve func to find zeros and poles
+[numerator1, denominator1] = numden(X);
+zeros1 = solve(numerator1 == 0);
+poles1 = solve(denominator1 == 0);
+
+subplot(1,3,1);
+zplane(zeros1, poles1); % zero-pole plot for X(z)
+title('X(z)');
+
+
+% part 2 - Question 2.1
+X2(z) = X(2*z); % X(2z)
+% we use numden function and after that solve func to find zeros and poles
+[numerator2, denominator2] = numden(X2);
+zeros2 = solve(numerator2 == 0);
+poles2 = solve(denominator2 == 0);
+
+subplot(1,3,2);
+zplane(zeros2, poles2); % zero-pole plot for X(2z)
+title('X(2z)');
+
+% inverse z transition for X2
+xinverse1(n) = iztrans(X2);
+
+
+% part 3 - Question 2.1
+X3 = X(z^3);% X(z^3)
+% we use numden function and after that solve func to find zeros and poles
+[numerator3, denominator3] = numden(X3);
+zeros3 = solve(numerator3 == 0);
+poles3 = solve(denominator3 == 0);
+
+
+subplot(1,3,3);
+zplane(zeros3(3:6), poles3); % zero-pole plot for X(z^3)
+title('X(z^3)');
+%I have just used zeros(3:6) because ine the numerator of X3 there is a z^3
+%and z^3 = 0 would put three zeros in out vector and this cause errors if
+%we use zeros3(1:6) while plotting zeros
+
+% inverse z transition for X3
+xinverse2(n) = iztrans(X3);
+
+% inverse z transitions` plot
+figure;
+subplot(2,1,1);
+ezplot(n,xinverse1);
+subplot(2,1,2);
+ezplot(n,xinverse2);
+
+ 
+end
+
