71bbc47c6f9124aad63fb54f93be80093e438da0
Your Name <arminpp1379@gmail.com>
2021-04-03 23:35:37 +0430

    Question 1.1 and 1.2 added

---
+++ HW2/Edge_Detection.m
@@ -0,0 +1,23 @@
+%Question.1.2
+% introduction to an edge detection algorithm for digital pictures called
+% Soble - Feldman which is a first order method
+ 
+clear; clc;
+
+%Question.1.1.2
+% load the picture
+pic1 = imread('NY_Q.1.2.jpg');
+%imshow(pic1);
+% define the kernels
+kernel_1 = [-1 0 1; -2 0 2; -1 0 1];
+kernel_2 = [-1 -2 -1; 0 0 0; 1 2 1];
+
+% use rgb2gray two find intensity of each pixel 
+% comment rgb2gray if you`re photo is already grayscale and just leave pic1
+Gx = conv2(kernel_1, rgb2gray(pic1));
+Gy = conv2(kernel_2, rgb2gray(pic1));
+
+G = sqrt(Gx.^2 + Gy.^2);
+
+% normalize and print the picture
+imshow(G/max(G(:)));
---
+++ HW2/ImageProcessing_Introduction.m
@@ -0,0 +1,46 @@
+%Question 1.1
+% Introduction to some simples function of image processing
+clear; clc;
+
+%Question.1.1.1
+% load the picture
+pic1 = imread('Bird_Q.1.1.jpg');
+
+% RGB split
+redChannel = pic1(:,:,1);
+greenChannel = pic1(:,:,2);
+blueChannel = pic1(:,:,3);
+
+
+% image and three splited channels
+subplot(2,2,1);
+imshow(pic1);
+title('original picture','interpreter','latex');
+subplot(2,2,2);
+imshow(blueChannel);
+title('Blue Channel','interpreter','latex');
+subplot(2,2,3);
+imshow(redChannel);
+title('Red Channel','interpreter','latex');
+subplot(2,2,4);
+imshow(greenChannel);
+title('green Channel','interpreter','latex');
+
+
+%Question.1.1.2
+figure;
+sum = blueChannel + redChannel + greenChannel;
+subplot(1,2,1);
+imshow(sum);
+title('sum of RGB channels','interpreter','latex');
+subplot(1,2,2);
+imshow(rgb2gray(pic1));
+title('RGB to Gray using rgb2gray','interpreter','latex');
+
+%Question.1.1.3
+% make the grayscale form of the picture binary
+figure;
+binarypic1 = imbinarize(rgb2gray(pic1));
+imshowpair(pic1,binarypic1,'montage');
+
+
