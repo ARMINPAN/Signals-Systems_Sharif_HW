85041a282718a72876e1afa7b284e4a6c6ae4450
Your Name <arminpp1379@gmail.com>
2021-04-18 01:54:21 +0430

    video changed

--- HW2/Q1_4_3_VideoPersonDetection.m
+++ HW2/Q1_4_3_VideoPersonDetection.m
@@ -2,35 +2,30 @@
 % detect people on a crosswalk video
 
 clc; clear;
-v=VideoReader('crosswalk.webm')
+v=VideoReader('crossWalkSelectedFrames.avi') % just 30 frames of the original file
 t=0;
-rng('default');
-selectedFrames = randperm(v.NumFrames,30); % use just 30 frames
-
 
 while hasFrame(v)
     filename = 'saved_gif.gif';
     I = readFrame(v);
     del = 0.001;
     % Your code for person detection
-    if(ismember(t,selectedFrames)) % use just 30 frames
-        imshow(detect(I));
-        frame = getframe(1);
-        im = frame2im(frame);
-        [imind,cm] = rgb2ind(im,256);
-        if t == 0
-            imwrite(imind,cm,filename,'gif','Loopcount',inf,'DelayTime',del);
-        else
-            imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',del);
-        end
+    imshow(detect(I));
+    frame = getframe(1);
+    im = frame2im(frame);
+    [imind,cm] = rgb2ind(im,256);
+    if t == 0
+        imwrite(imind,cm,filename,'gif','Loopcount',inf,'DelayTime',del);         
+    else
+        imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',del);
     end
-        t=t+1;
+    t=t+1;
 end
 
 
 function I = detect(frame)
 
-    thresh = 1.6; % example
+    thresh = 1.7; % example
     peopleDetector = vision.PeopleDetector('UprightPeople_96x48','ClassificationThreshold',thresh);
     I = frame;
     [bboxes,scores] = peopleDetector(I);
