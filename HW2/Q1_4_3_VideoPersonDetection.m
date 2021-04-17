097c1f368a8778c01e497ea56ce6a76b85ff2609
Your Name <arminpp1379@gmail.com>
2021-04-17 22:59:11 +0430

    detect people on the video

---
+++ HW2/Q1_4_3_VideoPersonDetection.m
@@ -0,0 +1,36 @@
+% question.1.4.3
+% detect people on a crosswalk video
+
+clc; clear;
+v=VideoReader('crosswalk.mp4');
+t=0;
+rng('default');
+selectedFrames = randperm(225,30); % use just 30 frames
+while hasFrame(v)
+    filename = 'saved_gif.gif';
+    I = readFrame(v);
+    del = 0.001;
+    % Your code for person detection
+    if(ismember(t,selectedFrames)) % use just 30 frames
+        imshow(detect(I));
+        frame = getframe(1);
+        im = frame2im(frame);
+        [imind,cm] = rgb2ind(im,256);
+        if t == 0
+            imwrite(imind,cm,filename,'gif','Loopcount',inf,'DelayTime',del);
+        else
+            imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',del);
+        end
+    end
+        t=t+1;
+end
+
+
+function I = detect(frame)
+
+    thresh = 1.7; % example
+    peopleDetector = vision.PeopleDetector('UprightPeople_96x48','ClassificationThreshold',thresh);
+    I = frame;
+    [bboxes,scores] = peopleDetector(I);
+    I = insertObjectAnnotation(I,'rectangle',bboxes,scores);
+end
