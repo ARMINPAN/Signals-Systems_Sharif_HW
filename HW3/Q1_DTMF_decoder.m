a72b69355745d0aef11f68a2b6298736d7b3ccd7
Your Name <arminpp1379@gmail.com>
2021-05-19 04:36:06 +0430

    DTMF for 0/10/20/30 db noisy signals

--- HW3/Q1_DTMF_decoder.m
+++ HW3/Q1_DTMF_decoder.m
@@ -2,41 +2,58 @@
 % question.1
 
 %% part 1
+% you should import the filters in workspace at first to run code
+
 clc;
-%1. no noise audio
-[audioNoNoise, Fs1] = audioread('DialedSequence_NoNoise.wav');
+% for other recorded signals change the input of audioread:
+% DialedSequence_NoNoise.wav
+% DialedSequence_SNR00dB.wav
+% DialedSequence_SNR10dB.wav
+% DialedSequence_SNR20dB.wav
+% DialedSequence_SNR30dB.wav
+
+[audio, Fs1] = audioread('DialedSequence_NoNoise.wav');
 
 % apply the filters
-bp697 = conv(BP_697.Numerator, audioNoNoise);
-bp770 = conv(BP_770.Numerator, audioNoNoise);
-bp852 = conv(BP_852.Numerator, audioNoNoise);
-bp941 = conv(BP_941.Numerator, audioNoNoise);
-bp1209 = conv(BP_1209.Numerator, audioNoNoise);
-bp1336 = conv(BP_1336.Numerator, audioNoNoise);
-bp1477 = conv(BP_1477.Numerator, audioNoNoise);
-bp1633 = conv(BP_1633.Numerator, audioNoNoise);
+bp697 = conv(BP_697.Numerator, audio);
+bp770 = conv(BP_770.Numerator, audio);
+bp852 = conv(BP_852.Numerator, audio);
+bp941 = conv(BP_941.Numerator, audio);
+bp1209 = conv(BP_1209.Numerator, audio);
+bp1336 = conv(BP_1336.Numerator, audio);
+bp1477 = conv(BP_1477.Numerator, audio);
+bp1633 = conv(BP_1633.Numerator, audio);
 
 Filtered_signal = bp697+bp770+bp852+bp941+bp1209+bp1336+bp1477+bp1633;
 
+subplot(2,1,1);
+plot(audio);
+title('Original Signal','interpreter','latex');
+xlabel('Sample','interpreter','latex');
+ylabel('Amplitude','interpreter','latex');
+subplot(2,1,2);
 plot(Filtered_signal);
+title('Filtered Signal','interpreter','latex');
+xlabel('Sample','interpreter','latex');
+ylabel('Amplitude','interpreter','latex');
 
 % detect whether a DTMF is presented at a particullar time or not by
 % setting a threshold C 
 % if the signal is more than the threshold in any time, we relize that a
 % key is pressed
-% by looking at the smoothed plots we realize that C = 0.25 is a good
-% threshold for no noise audio
+% by looking at the smoothed plots we realize that C = 0.15 is a good
+% threshold 
 
+bp697_pressed = find(bp697 > 0.15);
+bp770_pressed = find(bp770 > 0.15);
+bp852_pressed = find(bp852 > 0.15);
+bp941_pressed = find(bp941 > 0.15);
+bp1209_pressed = find(bp1209 > 0.15);
+bp1336_pressed = find(bp1336 > 0.15);
+bp1477_pressed = find(bp1477 > 0.15);
+bp1663_pressed = find(bp1633 > 0.15);
 
-bp697_pressed = find(bp697 > 0.3);
-bp770_pressed = find(bp770 > 0.3);
-bp852_pressed = find(bp852 > 0.3);
-bp941_pressed = find(bp941 > 0.3);
-bp1209_pressed = find(bp1209 > 0.3);
-bp1336_pressed = find(bp1336 > 0.3);
-bp1477_pressed = find(bp1477 > 0.3);
-bp1663_pressed = find(bp1633 > 0.3);
-
+% find intersection of the sets
 one_pressed = min(intersect(bp697_pressed, bp1209_pressed));
 two_pressed = min(intersect(bp697_pressed, bp1336_pressed));
 three_pressed = min(intersect(bp697_pressed, bp1477_pressed));
@@ -57,6 +74,7 @@ zero_pressed = min(intersect(bp941_pressed, bp1336_pressed));
 hashtag_pressed = min(intersect(bp941_pressed, bp1477_pressed));
 D_pressed = min(intersect(bp941_pressed, bp1663_pressed));
 
+% order of pressed keys
 Order = sort([one_pressed, two_pressed, three_pressed, A_pressed, ...
     four_pressed, five_pressed, six_pressed, B_pressed, seven_pressed, ...
     eight_pressed, nine_pressed, C_pressed, star_pressed, zero_pressed, ...
